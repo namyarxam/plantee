9d16372bf2afff8401360cb67f2278f05b60fd26
github username <github email address>
2016-03-17 10:40:24 -0400

    Updated bundle with watchify. Npm install watchify before running

---
+++ public/js/components/header.js
@@ -0,0 +1,13 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var auth = require('../helpers/auth')
+
+
+const Header = React.createClass({
+  render : function() {
+    return <h1>Header</h1>
+  }
+})
+
+module.exports = Header;
